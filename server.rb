0ad0b551ec7da8c04ed236068913f9d22c092aed
namdoiz <nnamdi.on7@gmail.com>
2023-02-16 16:01:47 -0600

    sending a complete response

--- server.rb
+++ server.rb
@@ -14,33 +14,39 @@ def parse_request(request_line)
   end
 
   [http_method, path, params]
-  
+
 end
 
 server= TCPServer.new("localhost", 3003)
+
 loop do
   client = server.accept
 
   request_line = client.gets
-
-  puts request_line
-
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
-
-  client.puts request_line
+  next if !request_line || request_line =~ /favicon/
 
   http_method = parse_request(request_line)[0]
   path = parse_request(request_line)[1]
   params = parse_request(request_line)[2]
 
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Rolls!</h2>"
 
   params["rolls"].to_i.times do
     client.puts rand(params["sides"].to_i + 1)
   end
 
-  client.puts(http_method)
-  client.puts(path)
-  client.puts(params)
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
 end
-
